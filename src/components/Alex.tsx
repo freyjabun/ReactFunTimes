5b86649004adecd6b8546791032e2196c736ee13
freyjabun <freyjabun@gmail.com>
2022-07-08 21:44:41 +0200

    alex is done

--- src/components/Alex.tsx
+++ src/components/Alex.tsx
@@ -1,24 +1,38 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, ChangeEventHandler, useState } from 'react'
 import './Alex.css'
 
 interface IAlexProps {
-    
+
 }
 
-const Alex: React.FC<IAlexProps>  = ({ }) => {
-    const [name, setName] = useState('Lars'); 
+const Alex: React.FC<IAlexProps> = ({ }) => {
+    const defaultName = 'Lars'
+    const [displayTextField, setTextFieldVisible] = useState(false)
+    const [name, setName] = useState(defaultName);
+    const [hiddenName, setHiddenName] = useState(defaultName);
+
+    const myFunction = () => {
+        if (!displayTextField) setTextFieldVisible(true)
+        else {
+            setName(hiddenName);
+            setTextFieldVisible(false);
+        }
+    }
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setHiddenName(event.target.value);
+    }
+
     return (
         <>
-        <h1>{name}</h1>
-        <button className='skrrt' onClick={() => setName(updateName(name))}></button>
-        </>    
+            <h1>{name}</h1>
+            <div className='coolField'>
+                {displayTextField ? <input className='coolInputField' type="text" onChange={handleChange}></input> : null}
+                <button className='skrrt' onClick={() => myFunction()}></button>
+            </div>
+        </>
     )
 }
 
-const updateName = (name: string) => {
-    if (name == 'Lars' ) name = 'Mads'
-    else name = 'Lars'
 
-    return name
-}
 export default Alex;

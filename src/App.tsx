db70023bbbaf3491183de73e1da510c6499602fc
erika <erika.maksimova@gmail.com>
2022-07-08 21:40:06 +0200

    routers yayyy

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Bob from './components/Bob';
-import Alex from './components/Alex';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import RedButton from "./pages/RedButtonLmao";
+import Navbar from "./Navbar";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <Alex></Alex>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/button' element={<RedButton/>} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 

f466b8a455a604952cb193f564135b3c3b45ca2c
erika <erika.maksimova@gmail.com>
2022-07-09 09:06:19 +0200

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import RedButton from "./pages/RedButtonLmao";
-import Navbar from "./Navbar";
+import NavbarComponent from "./NavbarComponent";
 
 export function App() {
   return (
     <BrowserRouter>
-        <Navbar />
+        <NavbarComponent />
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/button' element={<RedButton/>} />
